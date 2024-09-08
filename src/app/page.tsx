4afe678092194de72cf35bd1b0632f6160c68a8a
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-09-08 18:37:59 -0400

    whoops
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
   const cookieStore = cookies()
   var auth = cookieStore.get('token')?.value
 
-  LogUtility.logEntry(LogUtility.buildLogContext(), [
+  await LogUtility.logEntry(await LogUtility.buildLogContext(), [
     {
       severity: "INFO",
       jsonPayload: {
