0f9a00f09b1a38ea4b42dcb10ebe08a702c43796
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-10-30 16:39:55 -0400

    test
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/cloudflare_d1_ids.tf
+++ terraform/cloudflare_d1_ids.tf
@@ -7,7 +7,7 @@ resource "null_resource" "d1_pulse_ui_dev_cache_id" {
   provisioner "local-exec" {
     command = "${path.module}/scripts/get_d1_id.sh"
     environment = {
-      d1_name               = "pulse-ui-dev-cache"
+      d1_name               = "pulse_ui_dev_cache"
       cloudflare_account_id = var.cloudflare_account_id
       cloudflare_token      = var.cloudflare_token
     }
@@ -15,7 +15,7 @@ resource "null_resource" "d1_pulse_ui_dev_cache_id" {
 }
 
 data "local_file" "load_d1_pulse_ui_dev_cache_id" {
-  filename   = "${path.module}/pulse-ui-dev-cache"
+  filename   = "${path.module}/pulse_ui_dev_cache"
   depends_on = [null_resource.d1_pulse_ui_dev_cache_id]
 }
 
@@ -28,7 +28,7 @@ resource "null_resource" "d1_pulse_ui_prod_cache_id" {
   provisioner "local-exec" {
     command = "${path.module}/scripts/get_d1_id.sh"
     environment = {
-      d1_name               = "pulse-ui-prod-cache"
+      d1_name               = "pulse_ui_prod_cache"
       cloudflare_account_id = var.cloudflare_account_id
       cloudflare_token      = var.cloudflare_token
     }
@@ -36,6 +36,6 @@ resource "null_resource" "d1_pulse_ui_prod_cache_id" {
 }
 
 data "local_file" "load_d1_pulse_ui_prod_cache_id" {
-  filename   = "${path.module}/pulse-ui-prod-cache"
+  filename   = "${path.module}/pulse_ui_prod_cache"
   depends_on = [null_resource.d1_pulse_ui_prod_cache_id]
 }
