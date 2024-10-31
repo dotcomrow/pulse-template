3540b500325cc35f3b5113dd88dacf59fb354413
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-10-31 16:10:18 -0400

    fixing config
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const { setupDevPlatform } = require('@cloudflare/next-on-pages/next-dev');
-import withPWA from 'next-pwa';
+import withPWAInit from 'next-pwa';
 
 if (process.env.NODE_ENV === 'development') {
   setupDevPlatform();
@@ -14,10 +14,11 @@ const nextConfig = {
     }
 };
 
-export default withPWA({
+export default withPWAInit({
     dest: "public",         // destination directory for the PWA files
     //disable: process.env.NODE_ENV === "development",        // disable PWA in the development environment
     register: true,         // register the PWA service worker
     skipWaiting: true,      // skip waiting for service worker activation
     sw: "sw.js",    // service worker file name
+    buildExcludes: ["app-build-manifest.json"],
 })(nextConfig);
