4b7c4b885ed761b57bd8991947cb0a58d4893ae6
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-10-31 15:40:38 -0400

    adding intial service worker
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import 'tailwindcss/tailwind.css';
 import { Providers } from "./Providers";
 import { cookies } from 'next/headers'
@@ -6,13 +6,36 @@ import Header from "@component/layout/header/Header";
 import Footer from "@component/layout/footer/Footer";
 import "ol/ol.css";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from "next/script";
 
 export const runtime = 'edge';
 
 export const metadata: Metadata = {
   title: "Create Next App",
   description: "Generated with create app",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "next14", "pwa", "next-pwa"],
+  authors: [
+    {
+      name: "DotcomRow",
+      url: "https://www.linkedin.com/in/dotcomrow",
+    },
+  ],
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
 };
+
+
+export const viewport: Viewport = {
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  initialScale: 1,
+  minimumScale: 1,
+  width: "device-width",
+  viewportFit: "cover",
+}
 
 export default async function RootLayout({
   children,
@@ -33,6 +56,26 @@ export default async function RootLayout({
         </Providers>
       </body>
       <GoogleAnalytics gaId="G-8MHBD6Z0FG" />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+            if ("serviceWorker" in navigator) {
+              // Register a service worker hosted at the root of the
+              // site using the default scope.
+              navigator.serviceWorker.register("./sw.js").then(
+                (registration) => {
+                  console.log("Service worker registration succeeded:", registration);
+                },
+                (error) => {
+                  console.error('Service worker registration failed:', error);
+                }
+              );
+            } else {
+              console.error("Service workers are not supported.");
+            }
+          `,
+        }}
+      />
     </html>
   );
 }
