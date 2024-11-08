1bd43afe5d6e479e12fa23a5320091a8162836e6
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-11-07 21:38:31 -0500

    removing logs
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/components/map/components/ActivityTable.tsx
+++ src/components/map/components/ActivityTable.tsx
@@ -36,8 +36,6 @@ export default function ActivityTable({
                 changed = true;
             }
         });
-        console.log(items);
-        console.log(changed);
         if (changed) {
             setListItems(items);
         }
