6784049c94eb5193d715088582f80a3f70347bd7
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2024-11-10 09:06:37 -0500

    test
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/components/map/components/ActivityTable.tsx
+++ src/components/map/components/ActivityTable.tsx
@@ -9,6 +9,7 @@ import Feature from "ol/Feature";
 import Point from "ol/geom/Point";
 import { selectDeviceLocation } from "@lib/features/location/deviceLocationSlice";
 import { Spinner } from "@nextui-org/spinner";
+import "@styles/activityTable/iconImage.css"
 
 export default function ActivityTable({
 
@@ -18,8 +19,8 @@ export default function ActivityTable({
 
     const pictureRequestsState: any = useAppSelector(selectPictureRequests);
     const pictureRequestStatus: string = useAppSelector(selectPictureRequestStatus);
-    const deviceLocationState: any = useAppSelector(selectDeviceLocation);   
-    
+    const deviceLocationState: any = useAppSelector(selectDeviceLocation);
+
     const getDistance = (item: Feature) => {
         return (
             <>
@@ -49,27 +50,6 @@ export default function ActivityTable({
         return deg * (Math.PI / 180);
     }
 
-    const ImageIcon = () => (
-        <>
-            <div className="lg:hidden max-lg:flex">
-                <Image
-                    src="/assets/images/icons/camera.svg"
-                    alt="Camera Icon"
-                    width={50}
-                    height={50}
-                />
-            </div>
-            <div className="max-lg:hidden lg:flex">
-                <Image
-                    src="/assets/images/icons/camera.svg"
-                    alt="Camera Icon"
-                    width={25}
-                    height={25}
-                />
-            </div>
-        </>
-    );
-
     const ListboxWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => (
         <div className="w-full">
             {children}
@@ -86,14 +66,12 @@ export default function ActivityTable({
             </div>
         ) : (
             <ListboxWrapper>
-                <Listbox 
-                    variant="flat" 
+                <Listbox
+                    variant="flat"
                     aria-label="Listbox menu with sections"
                     items={pictureRequestsState}
                 >
                     {(item: Feature) => (
-                        <>
-                        <ImageIcon />
                         <ListboxItem
                             key={item.getId() ?? 'default-key'}
                             title={item.getProperties().request_title}
@@ -107,12 +85,14 @@ export default function ActivityTable({
                             }
                             textValue={item.getProperties().request_title}
                             startContent={
-                                <div className="image-placeholder" style={{ display: "none" }} /> // Use a placeholder for spacing
+                                <>
+                                    <div className="icon-background-small max-lg:hidden lg:flex" />
+                                    <div className="icon-background-large lg:hidden max-lg:flex" />
+                                </>
                             }
                         >
                             {item.getProperties().request_title}
                         </ListboxItem>
-                        </>
                     )}
                 </Listbox>
             </ListboxWrapper>
